ac4976b19f7083528113dfe0a66d99c671abf83a
Ning Shi <nshi@voltdb.com>
2013-05-20 17:53:03 -0400

    Proper commenting style in map.js.

--- src/ui/map.js
+++ src/ui/map.js
@@ -8,7 +8,7 @@ var dirServ = new google.maps.DirectionsService();
 function initialize() {
   var mapOptions = {
     zoom: 11,
-    center: new google.maps.LatLng(42.362096,-71.063004), <!-- Boston -->
+    center: new google.maps.LatLng(42.362096,-71.063004), // Boston
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
@@ -23,7 +23,7 @@ function initialize() {
 
 function setupMarkers() {
   startMarker = new google.maps.Marker({
-      position: new google.maps.LatLng(42.374588,-71.117291), <!-- Harvard -->
+      position: new google.maps.LatLng(42.374588,-71.117291), // Harvard
       map: map,
       title: "Start from here",
       animation: google.maps.Animation.DROP,
@@ -35,7 +35,7 @@ function setupMarkers() {
   });
 
   endMarker = new google.maps.Marker({
-      position: new google.maps.LatLng(42.359686,-71.094289), <!-- MIT -->
+      position: new google.maps.LatLng(42.359686,-71.094289), // MIT
       map: map,
       title: "End here",
       animation: google.maps.Animation.DROP,
@@ -48,33 +48,33 @@ function setupMarkers() {
 }
 
 function placeStartMarker(location) {
-  <!-- var contentString = '<div id="content">'+ -->
-  <!--   '<div id="siteNotice">'+ -->
-  <!--   '</div>'+ -->
-  <!--   '<h2 id="firstHeading" class="firstHeading">Uluru</h2>'+ -->
-  <!--   '<div id="bodyContent">'+ -->
-  <!--   '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' + -->
-  <!--   'sandstone rock formation in the southern part of the '+ -->
-  <!--   'Northern Territory, central Australia. It lies 335 km (208 mi) '+ -->
-  <!--   'south west of the nearest large town, Alice Springs; 450 km '+ -->
-  <!--   '(280 mi) by road. Kata Tjuta and Uluru are the two major '+ -->
-  <!--   'features of the Uluru - Kata Tjuta National Park. Uluru is '+ -->
-  <!--   'sacred to the Pitjantjatjara and Yankunytjatjara, the '+ -->
-  <!--   'Aboriginal people of the area. It has many springs, waterholes, '+ -->
-  <!--   'rock caves and ancient paintings. Uluru is listed as a World '+ -->
-  <!--   'Heritage Site.</p>'+ -->
-  <!--   '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+ -->
-  <!--   'http://en.wikipedia.org/w/index.php?title=Uluru</a> (last visited June 22, 2009).</p>'+ -->
-  <!--   '</div>'+ -->
-  <!--   '</div>'; -->
+  // var contentString = '<div id="content">'+
+  //   '<div id="siteNotice">'+
+  //   '</div>'+
+  //   '<h2 id="firstHeading" class="firstHeading">Uluru</h2>'+
+  //   '<div id="bodyContent">'+
+  //   '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+  //   'sandstone rock formation in the southern part of the '+
+  //   'Northern Territory, central Australia. It lies 335 km (208 mi) '+
+  //   'south west of the nearest large town, Alice Springs; 450 km '+
+  //   '(280 mi) by road. Kata Tjuta and Uluru are the two major '+
+  //   'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+  //   'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+  //   'Aboriginal people of the area. It has many springs, waterholes, '+
+  //   'rock caves and ancient paintings. Uluru is listed as a World '+
+  //   'Heritage Site.</p>'+
+  //   '<p>Attribution: Uluru, <a href="http://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+  //   'http://en.wikipedia.org/w/index.php?title=Uluru</a> (last visited June 22, 2009).</p>'+
+  //   '</div>'+
+  //   '</div>';
 
-  <!-- var infowindow = new google.maps.InfoWindow({ -->
-  <!--   content: contentString -->
-  <!-- }); -->
+  // var infowindow = new google.maps.InfoWindow({
+  //   content: contentString
+  // });
 
-  <!-- google.maps.event.addListener(startMarker, 'click', function() { -->
-  <!--   infowindow.open(map, startMarker); -->
-  <!-- }); -->
+  // google.maps.event.addListener(startMarker, 'click', function() {
+  //   infowindow.open(map, startMarker);
+  // });
 
   startMarker.setPosition(location);
 
